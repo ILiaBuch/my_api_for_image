67e374f2e053cdc818e0d8995c454dd4841245b6
ILiaBuch <liab@elephantheadsoft.com>
2016-06-05 09:54:07 +0300

    index in users ctrl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-   before_action :be_current_user
+   before_action :be_current_user, only: [:create]
    respond_to :json
    def index
+     if current_user
+      @user = current_user
+      render json: { access_key: @user.access_key }, status: :ok
+    else
+      render json: { errors: [message:  "You don not have  acces_key, for get acces_key have to make POST request on /users.json" , status: 401 ] }, status: :unauthorized
+    end
    end
 
    def create
